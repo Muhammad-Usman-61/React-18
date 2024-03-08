703c11910819f7c384396e9aacd661c534243f5d
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-09 00:09:32 +0500

    Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,31 @@
-const items = ["Profile", "Settings", "Messages", "Download"];
+import { MouseEvent } from "react";
+let items = ["Profile", "Settings", "Messages", "Download"];
+
+// Event handler
+const EventHandler = (event: MouseEvent) => {
+  console.log("Clicked on", event, "at index", event);
+};
+
 function ListGroup() {
   return (
     <>
       <h1>My list</h1>
-      <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        {items.map((item) => (
-          <li key={item} className="w-full px-4 py-2 rounded-b-lg">
-            {item}
-          </li>
-        ))}
-      </ul>
+
+      {items.length > 0 ? (
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+          {items.map((item, index) => (
+            <li
+              key={item}
+              className="w-full px-4 py-2 rounded-b-lg"
+              onClick={EventHandler}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No items to display</p>
+      )}
     </>
   );
 }
