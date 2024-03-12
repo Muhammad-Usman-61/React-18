f12b133552b3949b3382303d867f26cf44198ad0
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-12 23:32:33 +0500

    Alert component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={["Multan", "Sialkot", "Vehari", "Islamabd", "Karachi"]}
         heading="Cities of Pakistan"
         onSelection={(item, index) => console.log(item, index)}
-      />
+      /> */}
     </div>
   );
 }
