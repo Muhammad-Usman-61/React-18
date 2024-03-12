fe32b565530e20877c995891858b43d2591c18b0
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-13 00:10:31 +0500

    onclick, optional added to button component

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,14 @@ function App() {
         color="text-white"
         backgroundColor="bg-blue-700"
         onHoverBackgroundColor="bg-blue-800"
+        onClick={() => console.log("1st Button clicked")}
       />
       <Button
         text="Deafault"
         color="text-white"
         backgroundColor="bg-gray-800"
         onHoverBackgroundColor="bg-gray-900"
+        onClick={() => console.log("2nd Button clicked")}
       />
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,25 @@
 interface Props {
   text: string;
-  color: string;
-  backgroundColor: string;
+  color?: "text-white" | "text-gray-800" | "text-gray-900" | "text-black";
+  backgroundColor?: "bg-blue-700" | "bg-gray-800" | "bg-red-700";
   onHoverBackgroundColor: string;
+  onClick: () => void;
 }
 function Button({
   text,
-  color,
-  backgroundColor,
+  color = "text-white",
+  backgroundColor = "bg-blue-700",
   onHoverBackgroundColor,
+  onClick,
 }: Props) {
   return (
-    <div>
-      <button
-        type="button"
-        className={`${color} ${backgroundColor} hover:${onHoverBackgroundColor} font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${color} ${backgroundColor} hover:${onHoverBackgroundColor} font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+    >
+      {text}
+    </button>
   );
 }
 
