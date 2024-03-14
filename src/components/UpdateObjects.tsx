540a8275a702cee18125debdce96e2ded4b268a5
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-14 22:44:52 +0500

    update nested objects in useState

--- src/components/UpdateObjects.tsx
+++ src/components/UpdateObjects.tsx
@@ -9,16 +9,25 @@ function UpdateObjects({ name, age, updatedName }: Props) {
   const [objects, setObjects] = useState({
     name: name,
     age: age,
+    address: {
+      street: "1234 Main St",
+      city: "New York",
+      zip: 10001,
+    },
   });
 
   const updateName = () => {
-    setObjects({ ...objects, name: updatedName });
+    setObjects({
+      ...objects,
+      name: updatedName,
+      address: { ...objects.address, zip: 10002 },
+    });
   };
   return (
     <div className="flex gap-4 items-center mt-4 py-4 border-solid border-grey border-y-2">
-      {objects.name}
+      {objects.name} {objects.address.zip}
       <Button
-        text="Update Name"
+        text="Update Name & ZipCode"
         onHoverBackgroundColor="bg-blue-600"
         onClick={updateName}
       />
