0962cb17393e72599d7e38f4f7d9d281559f2e4b
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-15 05:12:45 +0500

    add interest section updated

--- src/components/UpdateArrays.tsx
+++ src/components/UpdateArrays.tsx
@@ -1,9 +1,11 @@
 import Button from "./Button";
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 function UpdateArrays() {
   const [tags, setTags] = useState(["React", "TypeScript", "TailwindCSS"]);
   const [newTag, setNewTag] = useState("");
+  const [showTagInput, setShowTagInput] = useState(false);
   const handleClick = () => {
     //Add a new tag to the array
     // setTags([...tags, "JavaScript"]);
@@ -15,25 +17,44 @@ function UpdateArrays() {
       setTags([...tags, newTag.trim()]);
       setNewTag("");
     }
+    setShowTagInput(!showTagInput);
+  };
+  const removeTag = (tag: string) => {
+    setTags(tags.filter((t) => t !== tag));
   };
   return (
-    <div className="flex gap-4 items-center py-4 border-solid border-grey border-b-2">
-      {tags.map((tag, index) => (
-        <span key={index} className="bg-blue-700 text-white p-2 rounded">
-          {tag}
-        </span>
-      ))}
-      <input
-        type="text"
-        value={newTag}
-        onChange={(e) => setNewTag(e.target.value)}
-        className="border border-gray-400 p-1"
-      />
-      <Button
-        text="Add Tag"
-        onHoverBackgroundColor="bg-blue-600"
-        onClick={handleClick}
-      />
+    <div className="flex gap-4 items-center p-4 max-w-96 shadow-md my-4 bg-white rounded-lg">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-lg font-semibold">Add Your Interests</h1>
+        <div className="flex gap-2 justify-between">
+          {showTagInput && (
+            <input
+              type="text"
+              value={newTag}
+              onChange={(e) => setNewTag(e.target.value)}
+              className="border border-gray-400 p-1"
+            />
+          )}
+          <Button
+            text="Add New"
+            onHoverBackgroundColor="bg-blue-600"
+            onClick={handleClick}
+          />
+        </div>
+        <div className="flex gap-2 flex-wrap">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="flex text-sm gap-2 items-center w-fit bg-blue-700 text-white p-2 rounded cursor-pointer hover:shadow-md hover:shadow-gray-400"
+            >
+              {tag}{" "}
+              <span onClick={() => removeTag(tag)}>
+                <IoClose />
+              </span>
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
