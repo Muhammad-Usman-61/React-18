3e93e57899a8aa7d73512aa686ed51f9d7666e8e
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-21 22:56:12 +0500

    form component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,132 @@
 // import ListGroup from "./components/ListGroup/ListGroup";
 // import Alert from "./components/Alert";
-import Button from "./components/Button";
+// import Button from "./components/Button";
 // import ShowAlert from "./components/showAlert";
 // import LikeButton from "./components/LikeButton";
 // import UpdateObjects from "./components/UpdateObjects";
 // import UpdateArrays from "./components/UpdateArrays";
 // import UpdateArrayOfObjects from "./components/UpdateArrayOfObjects";
-import { FaBowlFood } from "react-icons/fa6";
-import { MdNoDrinks } from "react-icons/md";
-import NavBar from "./components/navBar";
-import Cart from "./components/cart";
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UpdateState from "./components/Exercises/UpdateState";
-import AddString from "./components/Exercises/AddString";
-import UpdateObjectsInArray from "./components/Exercises/UpdateObjectsInArray";
-import ExpandableText from "./components/Exercises/ExpandableText";
+// import { FaBowlFood } from "react-icons/fa6";
+// import { MdNoDrinks } from "react-icons/md";
+// import NavBar from "./components/navBar";
+// import Cart from "./components/cart";
+// import { useState } from "react";
+// import SearchBar from "./components/SearchBar";
+// import UpdateState from "./components/Exercises/UpdateState";
+// import AddString from "./components/Exercises/AddString";
+// import UpdateObjectsInArray from "./components/Exercises/UpdateObjectsInArray";
+// import ExpandableText from "./components/Exercises/ExpandableText";
+import Form from "./components/Form";
 function App() {
-  const [foodItems, setFoodItems] = useState([
-    "Apple",
-    "Banana",
-    "Orange",
-    "Mango",
-    "Pineapple",
-  ]);
-  const [showFoodItems, setShowFoodItems] = useState(true);
-  const [showDrinkItems, setShowDrinkItems] = useState(false);
-  const [drinkItems, setDrinkItems] = useState([
-    "Coke",
-    "Pepsi",
-    "Sprite",
-    "Fanta",
-    "7up",
-  ]);
+  //   const [foodItems, setFoodItems] = useState([
+  //     "Apple",
+  //     "Banana",
+  //     "Orange",
+  //     "Mango",
+  //     "Pineapple",
+  //   ]);
+  //   const [showFoodItems, setShowFoodItems] = useState(true);
+  //   const [showDrinkItems, setShowDrinkItems] = useState(false);
+  //   const [drinkItems, setDrinkItems] = useState([
+  //     "Coke",
+  //     "Pepsi",
+  //     "Sprite",
+  //     "Fanta",
+  //     "7up",
+  //   ]);
 
-  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  //   const [filteredItems, setFilteredItems] = useState<string[]>([]);
 
-  const totalItemsList = foodItems.concat(drinkItems);
+  //   const totalItemsList = foodItems.concat(drinkItems);
 
-  const handleSearch = (query: string) => {
-    const filteredFoodItems = foodItems.filter((item) =>
-      item.toLowerCase().includes(query.toLowerCase())
-    );
-    const filteredDrinkItems = drinkItems.filter((item) =>
-      item.toLowerCase().includes(query.toLowerCase())
-    );
-    if (
-      filteredFoodItems.length === 0 &&
-      filteredDrinkItems.length === 0 &&
-      query !== ""
-    ) {
-      setFilteredItems(["No items found"]);
-    } else {
-      setFilteredItems([...filteredFoodItems, ...filteredDrinkItems]);
-    }
-  };
+  //   const handleSearch = (query: string) => {
+  //     const filteredFoodItems = foodItems.filter((item) =>
+  //       item.toLowerCase().includes(query.toLowerCase())
+  //     );
+  //     const filteredDrinkItems = drinkItems.filter((item) =>
+  //       item.toLowerCase().includes(query.toLowerCase())
+  //     );
+  //     if (
+  //       filteredFoodItems.length === 0 &&
+  //       filteredDrinkItems.length === 0 &&
+  //       query !== ""
+  //     ) {
+  //       setFilteredItems(["No items found"]);
+  //     } else {
+  //       setFilteredItems([...filteredFoodItems, ...filteredDrinkItems]);
+  //     }
+  //   };
 
   return (
     <div className="max-w-lg">
-      <SearchBar onSearch={handleSearch} />
-      <div className="border-b border-gray-200 flex items-center">
-        <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-          <li
-            onClick={() => {
-              setShowFoodItems(!showFoodItems), setShowDrinkItems(false);
-            }}
-            className={
-              showFoodItems
-                ? "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg text-gray-600 border-gray-300 group cursor-pointer"
-                : "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer"
-            }
-          >
-            <FaBowlFood />
-            Food Items
-            <span>({foodItems.length})</span>
-          </li>
+      {/*     <SearchBar onSearch={handleSearch} />
+       <div className="border-b border-gray-200 flex items-center">
+         <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+           <li 
+             onClick={() => {
+//               setShowFoodItems(!showFoodItems), setShowDrinkItems(false);
+//             }}
+//             className={
+//               showFoodItems
+//                 ? "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg text-gray-600 border-gray-300 group cursor-pointer"
+//                 : "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer"
+//             }
+//           >
+//             <FaBowlFood />
+//             Food Items
+//             <span>({foodItems.length})</span>
+//           </li>
 
-          <li
-            onClick={() => {
-              setShowDrinkItems(!showDrinkItems), setShowFoodItems(false);
-            }}
-            className={
-              showDrinkItems
-                ? "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg text-gray-600 border-gray-300 group cursor-pointer"
-                : "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer"
-            }
-          >
-            <MdNoDrinks />
-            Drink Items
-            <span>({drinkItems.length})</span>
-          </li>
-        </ul>
-        <NavBar cartItemsCount={totalItemsList.length} />
-      </div>
+//           <li
+//             onClick={() => {
+//               setShowDrinkItems(!showDrinkItems), setShowFoodItems(false);
+//             }}
+//             className={
+//               showDrinkItems
+//                 ? "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg text-gray-600 border-gray-300 group cursor-pointer"
+//                 : "gap-2 me-2 inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 group cursor-pointer"
+//             }
+//           >
+//             <MdNoDrinks />
+//             Drink Items
+//             <span>({drinkItems.length})</span>
+//           </li>
+//         </ul>
+//         <NavBar cartItemsCount={totalItemsList.length} />
+//       </div>
 
-      {showFoodItems && (
-        <Cart
-          products={filteredItems.length > 0 ? filteredItems : foodItems}
-          removeProduct={(product) => {
-            const newItems = foodItems.filter((item) => item !== product);
-            setFoodItems(newItems);
-          }}
-          filteredItems={() => setFilteredItems([])}
-        />
-      )}
-      {showDrinkItems && (
-        <Cart
-          products={filteredItems.length > 0 ? filteredItems : drinkItems}
-          removeProduct={(product) => {
-            const newItems = drinkItems.filter((item) => item !== product);
-            setDrinkItems(newItems);
-          }}
-          filteredItems={() => setFilteredItems([])}
-        />
-      )}
-      {totalItemsList.length !== 0 && (
-        <Button
-          text="Clear Cart"
-          color="text-white"
-          backgroundColor="bg-slate-800"
-          onHoverBackgroundColor="bg-slate-600"
-          onClick={() => {
-            setFoodItems([]);
-            setDrinkItems([]);
-          }}
-        />
-      )}
+//       {showFoodItems && (
+//         <Cart
+//           products={filteredItems.length > 0 ? filteredItems : foodItems}
+//           removeProduct={(product) => {
+//             const newItems = foodItems.filter((item) => item !== product);
+//             setFoodItems(newItems);
+//           }}
+//           filteredItems={() => setFilteredItems([])}
+//         />
+//       )}
+//       {showDrinkItems && (
+//         <Cart
+//           products={filteredItems.length > 0 ? filteredItems : drinkItems}
+//           removeProduct={(product) => {
+//             const newItems = drinkItems.filter((item) => item !== product);
+//             setDrinkItems(newItems);
+//           }}
+//           filteredItems={() => setFilteredItems([])}
+//         />
+//       )}
+//       {totalItemsList.length !== 0 && (
+//         <Button
+//           text="Clear Cart"
+//           color="text-white"
+//           backgroundColor="bg-slate-800"
+//           onHoverBackgroundColor="bg-slate-600"
+//           onClick={() => {
+//             setFoodItems([]);
+//             setDrinkItems([]);
+//           }}
+//         />
+//       )}
       {/* <ListGroup
         items={["Multan", "Sialkot", "Vehari", "Islamabad", "Karachi"]}
         heading="Cities of Pakistan"
@@ -161,7 +162,7 @@ function App() {
       <UpdateObjects name="Usman" age={23} updatedName="Ali" />
       <UpdateArrays />
       <UpdateArrayOfObjects /> */}
-      <UpdateState />
+      {/* <UpdateState />
       <AddString />
       <UpdateObjectsInArray />
       <ExpandableText maxLength={20}>
@@ -169,7 +170,8 @@ function App() {
         Eaque ullam expedita dolorem vitae dolorum. Excepturi, aut? Quis
         voluptas praesentium quidem dolores tenetur nisi dolore delectus
         voluptatibus optio sit!
-      </ExpandableText>
+      </ExpandableText> */}
+      <Form />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,26 @@
 interface Props {
   text: string;
+  type?: "button" | "submit" | "reset";
   color?: "text-white" | "text-gray-800" | "text-gray-900" | "text-black";
   backgroundColor?:
     | "bg-blue-700"
     | "bg-gray-800"
     | "bg-red-700"
     | "bg-slate-800";
-  onHoverBackgroundColor: string;
-  onClick: () => void;
+  onHoverBackgroundColor?: string;
+  onClick?: () => void;
 }
 function Button({
   text,
+  type = "button",
   color = "text-white",
   backgroundColor = "bg-blue-700",
-  onHoverBackgroundColor,
+  onHoverBackgroundColor = "bg-blue-800",
   onClick,
 }: Props) {
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       className={`${color} ${backgroundColor} hover:${onHoverBackgroundColor} font-medium rounded-lg text-sm px-5 py-2.5`}
     >
