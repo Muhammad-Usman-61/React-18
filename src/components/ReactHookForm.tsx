509358e3e83501b925b9f4385a86c1138fdd0430
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-21 23:58:40 +0500

    form validation

--- src/components/ReactHookForm.tsx
+++ src/components/ReactHookForm.tsx
@@ -1,10 +1,42 @@
 import { FieldValues, useForm } from "react-hook-form";
 import Button from "./Button";
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
 const ReactHookForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <form className="max-w-sm mx-auto" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-5">
+        <label
+          htmlFor="name"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Your email
+        </label>
+        <input
+          type="text"
+          id="emanameil"
+          {...(register("name"), { required: true, minLength: 3 })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="name@gmail.com"
+        />
+        {errors.name?.type === "required" && (
+          <span className="text-red-500 text-sm">This field is required</span>
+        )}
+        {errors.name?.type === "minLength" && (
+          <span className="text-red-500 text-sm">
+            This field should have at least 3 characters
+          </span>
+        )}
+      </div>
       <div className="mb-5">
         <label
           htmlFor="email"
@@ -15,10 +47,9 @@ const ReactHookForm = () => {
         <input
           type="email"
           id="email"
-          {...register("email")}
+          {...(register("email"), { required: true })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           placeholder="name@gmail.com"
-          required
         />
       </div>
       <div className="mb-5">
@@ -31,9 +62,8 @@ const ReactHookForm = () => {
         <input
           type="password"
           id="password"
-          {...register("password")}
+          {...(register("password"), { required: true, minLength: 5 })}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          required
         />
       </div>
 
