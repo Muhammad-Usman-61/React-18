af26058d19c90858bee27e83c76aee84392050ce
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-24 23:53:06 +0500

    table added in expense tracker

--- src/expense-tracker/components/ExpenseList.tsx
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  catagory: string;
+}
 
-const ExpenseList = () => {
-  return <div>ExpenseList</div>;
+interface Props {
+  expenses: Expense[];
+}
+const ExpenseList = ({ expenses }: Props) => {
+  return (
+    <table>
+      <thead>
+        <th>Description</th>
+        <th>Amount</th>
+        <th>Catagory</th>
+        <th></th>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.catagory}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default ExpenseList;
