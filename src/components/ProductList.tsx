2ebdc8e754ed9ee292e9d76b6483871aaffad902
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-27 21:47:37 +0500

    efferct clean up. e.g. connect and then disconnect when not needed

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 
-const ProductList = ({ catagory }: { catagory: string }) => {
-  const [products, setProducts] = useState<string[]>([]);
+// const ProductList = ({ catagory }: { catagory: string }) => {
+//   const [products, setProducts] = useState<string[]>([]);
+
+//   useEffect(() => {
+//     console.log("ProductList Fetching data from", catagory);
+//     setProducts(["Apple", "Banana", "Orange"]);
+//   }, [catagory]);
+//   return <div>ProductList</div>;
+// };
+
+const ProductList = () => {
+  const connect = () => console.log("Connecting to database");
+  const disconnect = () => console.log("Disconnecting from database");
 
   useEffect(() => {
-    console.log("ProductList Fetching data from", catagory);
-    setProducts(["Apple", "Banana", "Orange"]);
-  }, [catagory]);
-  return <div>ProductList</div>;
+    connect();
+    return () => disconnect();
+  });
 };
 
 export default ProductList;
