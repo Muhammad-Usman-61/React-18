30e7c07c1e7c83fcf962cd938d6fa636174b8cc8
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-03-30 08:07:56 +0500

    spinner added while fetching data using react-spinners

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 // const ProductList = ({ catagory }: { catagory: string }) => {
 //   const [products, setProducts] = useState<string[]>([]);
@@ -16,6 +17,7 @@ interface User {
 }
 const ProductList = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
   //   const connect = () => console.log("Connecting to database");
   //   const disconnect = () => console.log("Disconnecting from database");
   //   useEffect(() => {
@@ -24,33 +26,43 @@ const ProductList = () => {
   //   });
   const [error, setError] = useState("");
   useEffect(() => {
-    // axios
-    //   .get("https://jsonplaceholder.typicode.com/husers")
-    //   .then((res) => {
-    //     setUsers(res.data);
-    //   })
-    //   .catch((err) => setError(err.message));
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
         setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
-    fetchUsers();
+        setLoading(false);
+      })
+      .catch((err) => setError(err.message));
+    // const fetchUsers = async () => {
+    //   try {
+    //     const res = await axios.get<User[]>(
+    //       "https://jsonplaceholder.typicode.com/users"
+    //     );
+    //     setUsers(res.data);
+    //   } catch (err) {
+    //     setError((err as AxiosError).message);
+    //   }
+    // };
+    // fetchUsers();
   }, []);
 
   return (
     <>
       {error && <p className="text-red-600">{error}</p>}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {loading ? (
+        <ClipLoader
+          color="blue"
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
